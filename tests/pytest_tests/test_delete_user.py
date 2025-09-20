7c7552d7263a06464e9e70d5172ec7e36f3840f0
Tomislav Vuckovic <vuckovic.tomislav1@gmail.com>
2025-09-20 15:51:39 +0200

    test: refaktorera test_delete_user.py för ökad tydlighet

--- tests/pytest_tests/test_delete_user.py
+++ tests/pytest_tests/test_delete_user.py
@@ -1,25 +1,28 @@
-def test_delete_user(client_with_user_id):
-    client_with_user, user_id = client_with_user_id
+from app.models import db, User
 
-    response = client_with_user.delete(f'/users/{user_id}')
+def test_delete_user(client):
+    with client.application.app_context():
+        user = User(name='Fran', email='fran@example.com')
+        db.session.add(user)
+        db.session.commit()
+        user_id = user.id
+
+    # Ta bort användaren
+    response = client.delete(f'/users/{user_id}')
     assert response.status_code == 200
-
     data = response.get_json()
-    assert 'message' in data
-    assert data['message'] == f'User with id {user_id} deleted.'
+    expected = {'message': f'User with id {user_id} deleted.'}
+    assert data == expected
 
-    check = client_with_user.get(f'/users/{user_id}')
+    # Användaren finns inte kvar längre
+    check = client.get(f'/users/{user_id}')
     assert check.status_code == 404
 
-
-def test_delete_non_existing_user(client_with_user_id):
-    client_with_user, user_id = client_with_user_id
-
-    client_with_user.delete(f'/users/{user_id}')
-
-    response = client_with_user.delete(f'/users/{user_id}')
+def test_delete_non_existing_user_returns_404(client):
+    response = client.delete('/users/9999')
     assert response.status_code == 404
 
+    expected ={'error': 'User not found!'}
+
     data = response.get_json()
-    assert 'error' in data
-    assert data['error'] == 'User not found!'
+    assert data == expected
